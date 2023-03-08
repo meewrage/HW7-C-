dde659c4eece180fc51ad924fd7419687c9ab797
meewrage <luki_wop@mail.ru>
2023-03-09 00:16:47 +0300

    added task52

---
+++ task52/Program.cs
@@ -0,0 +1,67 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int ReadNumber(string messageToUser)     //метод считывания целых чисел
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix(int rows, int colums, int leftBorder = 0, int rightBorder = 10) // создает матрицу
+{
+ 
+    int[,] matrix = new int[rows, colums];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder +1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)  // выводит матрицу на консоль
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+float[,] averageMatrix(int [,] matrix)
+{
+    int count = 0;
+    int q = 0;
+         for(int j = 0; j < matrix.GetLength(1); j++ )
+    {
+        float av = count / q;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            count += matrix[i,j];
+            q += 1;
+
+        }
+
+    }
+       
+        
+           
+       return av; 
+}
+
+
+int m = ReadNumber("Введите кол-во строк");
+int n = ReadNumber("Введите кол-во столбцов");
+int[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
+Console.WriteLine(averageMatrix);
