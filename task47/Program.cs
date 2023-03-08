f8fdb2791f8ad017ef3916ee8c3526367ac49352
meewrage <luki_wop@mail.ru>
2023-03-08 23:21:04 +0300

    added task47

---
+++ task47/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+int ReadNumber(string messageToUser)     //метод считывания целых чисел
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+double[,] GetRandomMatrix(int rows, int colums, int leftBorder = -10, int rightBorder = 99) // создает матрицу
+{
+ 
+    double[,] matrix = new double[rows, colums];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder +1) * 0.01;
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)  // выводит матрицу на консоль
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+int m = ReadNumber("Введите кол-во строк");
+int n = ReadNumber("Введите кол-во столбцов");
+double[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
